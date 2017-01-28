404558ce350b1af1b85e6b79f7bcf1bcd0e99861
Dan Wright <muscle.crab.13@gmail.com>
2017-01-27 22:48:02 -0800

    single command runs the server, need to work out logging and dev setup though.

---
+++ site/ansible/create_logs.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+echo 'Make some log files'
+touch /var/log/site_uwsgi.log
+touch /var/log/access.log
+mkdir /var/log/nginx/
+touch /var/log/nginx/access.log
+touch /var/log/nginx/error.log
+touch /var/log/pip.log
--- site/ansible/startserver.sh
+++ site/ansible/startserver.sh
@@ -1,22 +1,20 @@
 #! /usr/bin/env bash
 
-echo 'Make some log files'
-touch /var/log/site_uwsgi.log
-touch /var/log/access.log
-mkdir /var/log/nginx/
-touch /var/log/nginx/access.log
-touch /var/log/nginx/error.log
-
 echo 'replace nginx configs'
+
 rm /etc/nginx/sites-enabled/default
+# above: fails all the time anyway. perhaps not needed?
+
 ln -s /opt/site/current/configs/site_nginx.conf /etc/nginx/conf.d/
+# above: handled in ansible?
+
 
 echo 'start nginx'
 /etc/init.d/nginx start
 nginx -t
 
 echo 'run ansible stuff..'
-ansible-playbook main.yml
+ansible-playbook ansible/main.yml --tags 'server,pip'
 
 echo 'start server'
 /opt/site/current/venv/bin/uwsgi --ini /opt/site/current/configs/site_uwsgi.ini
