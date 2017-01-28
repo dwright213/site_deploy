80607be8968460f0388881e050605bfb32d85443
Dan Wright <muscle.crab.13@gmail.com>
2017-01-28 14:08:33 -0800

    ansible script fixes n updates

--- site/ansible/startserver.sh
+++ site/ansible/startserver.sh
@@ -8,13 +8,14 @@ rm /etc/nginx/sites-enabled/default
 ln -s /opt/site/current/configs/site_nginx.conf /etc/nginx/conf.d/
 # above: handled in ansible?
 
+echo 'run ansible stuff..'
+# ansible-playbook ansible/main.yml --tags 'server,pip'
+ansible-playbook ansible/main.yml -v
 
 echo 'start nginx'
 /etc/init.d/nginx start
 nginx -t
 
-echo 'run ansible stuff..'
-ansible-playbook ansible/main.yml --tags 'server,pip'
 
 echo 'start server'
 /opt/site/current/venv/bin/uwsgi --ini /opt/site/current/configs/site_uwsgi.ini
